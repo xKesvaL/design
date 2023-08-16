c2d49b5f8c4695c7fa863e9539ec74649701df63
xKesvaL <jabeddou@gmail.com>
2023-08-16 21:42:46 +0200

    feat: setup, linters and package

---
+++ stylelint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: "stylelint-config-standard-scss",
+  plugins: ["stylelint-scss", "stylelint-order"],
+  rules: {
+    "declaration-property-unit-allowed-list": {
+      "/^border/": ["px"],
+      "/^padding|^gap/": ["rem"],
+    },
+    "unit-allowed-list": ["%", "deg", "rem", "ms"],
+  },
+};
